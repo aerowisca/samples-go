62af51de82df780a86bf69d1a81d90aac8756018
Aerowisca <aerowisca@gmail.com>
2023-07-18 22:57:33 +0530

    made conn as non-tls

--- grpc/server/server.go
+++ grpc/server/server.go
@@ -51,7 +51,7 @@ import (
 )
 
 var (
-	tls        = flag.Bool("tls", true, "Connection uses TLS if true, else plain TCP")
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
 	certFile   = flag.String("cert_file", "", "The TLS cert file")
 	keyFile    = flag.String("key_file", "", "The TLS key file")
 	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
